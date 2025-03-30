364da8a1a3ab2697ea47d9e625f0d2123107401c
Marina S Bon <marina.bon@edu.pucrs.br>
2025-03-30 10:30:05 -0300

    ajustes no endpoint categoria

--- src/main/java/com/curso/cursomc/cursomcResources/CategoriaResource.java
+++ src/main/java/com/curso/cursomc/cursomcResources/CategoriaResource.java
@@ -1,13 +1,14 @@
 package com.curso.cursomc.cursomcResources;
 
-import com.curso.cursomc.cursomcDomain.Categoria;
-import com.curso.cursomc.cursomcServices.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.curso.cursomc.cursomcDomain.Categoria;
+import com.curso.cursomc.cursomcServices.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
--- src/main/java/com/curso/cursomc/cursomcServices/CategoriaService.java
+++ src/main/java/com/curso/cursomc/cursomcServices/CategoriaService.java
@@ -1,10 +1,14 @@
 package com.curso.cursomc.cursomcServices;
 
-import com.curso.cursomc.cursomcDomain.Categoria;
-import com.curso.cursomc.cursomcRepositories.CategoriaRepository;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.curso.cursomc.cursomcDomain.Categoria;
+import com.curso.cursomc.cursomcRepositories.CategoriaRepository;
+import com.curso.cursomc.cursomcServices.cursomcExceptions.ObjectNotFoundException;
+
 
 @Service
 public class CategoriaService {
@@ -14,7 +18,8 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 
 }
