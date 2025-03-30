0b94c874085c35dc6a077b740b36988a7f6e5de4
Marina S Bon <marina.bon@edu.pucrs.br>
2025-03-30 09:47:23 -0300

    Produto added

--- src/main/java/com/curso/cursomc/cursomcRepositories/ProdutoRepository.java
+++ src/main/java/com/curso/cursomc/cursomcRepositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.curso.cursomc.cursomcRepositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.curso.cursomc.cursomcDomain.Categoria;
+import com.curso.cursomc.cursomcDomain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
     
 }
